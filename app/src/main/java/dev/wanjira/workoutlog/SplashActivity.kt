cc1e5b3490ad55f7471c2c9b9c95b3d95b7082dc
wanjira-whitney <wanjirawhitney@gmail.com>
2022-05-31 19:41:12 +0300

    first commit

---
+++ app/src/main/java/dev/wanjira/workoutlog/SplashActivity.kt
@@ -0,0 +1,15 @@
+package dev.wanjira.workoutlog
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        var intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
